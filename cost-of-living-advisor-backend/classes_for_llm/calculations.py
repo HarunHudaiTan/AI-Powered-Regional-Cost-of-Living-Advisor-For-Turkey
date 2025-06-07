2ca9634ec74c2cd056c1600479658e146cc2d660
HarunHudaiTan <harun2002tan@gmail.com>
2025-06-07 19:21:10 +0300

    comment out calculations

--- cost-of-living-advisor-backend/classes_for_llm/calculations.py
+++ cost-of-living-advisor-backend/classes_for_llm/calculations.py
@@ -23,11 +23,11 @@ def calculate_fuel_price_for_current_and_target_city(current_city, target_city):
     return current_fuel_price,target_city_fuel_price
 
 
-utility_prices=utilities.get_all_utility_prices(city_name)
-transportation_prices=transportation.get_all_transportation_prices(city_name)
-print(json.dumps(transportation_prices,indent=2, ensure_ascii=False,cls=DecimalEncoder))
-print(json.dumps(utility_prices, indent=2, ensure_ascii=False, cls=DecimalEncoder))
-transportation_prices=transportation.get_all_transportation_prices(city_name)
-print(calculate_average_price_for_water(utility_prices['utilities']['water']['price_per_m3']))
-print(calculate_average_price_for_natural_gas(utility_prices['utilities']['natural_gas']['price_per_m3']))
-print(calculate_average_price_for_electricity(utility_prices['utilities']['electricity']['tariffs'][0]['price_per_kwh']))
+# utility_prices=utilities.get_all_utility_prices(city_name)
+# transportation_prices=transportation.get_all_transportation_prices(city_name)
+# print(json.dumps(transportation_prices,indent=2, ensure_ascii=False,cls=DecimalEncoder))
+# print(json.dumps(utility_prices, indent=2, ensure_ascii=False, cls=DecimalEncoder))
+# transportation_prices=transportation.get_all_transportation_prices(city_name)
+# print(calculate_average_price_for_water(utility_prices['utilities']['water']['price_per_m3']))
+# print(calculate_average_price_for_natural_gas(utility_prices['utilities']['natural_gas']['price_per_m3']))
+# print(calculate_average_price_for_electricity(utility_prices['utilities']['electricity']['tariffs'][0]['price_per_kwh']))
